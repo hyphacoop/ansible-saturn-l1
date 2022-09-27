62faaa6016a1f65c346fa9a1992d5e50e5cdd5d3
YurkoWasHere <YurkoWasHere@users.noreply.github.com>
2022-09-27 18:16:15 -0400

    Saturn L1 Node

---
+++ roles/saturn_l1/templates/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export FIL_WALLET_ADDRESS="{{ SATURN_FILECOIN_ADDRESS }}"
+export NODE_OPERATOR_EMAIL="{{ SATURN_EMAIL }}"
+export SATURN_NETWORK="{{ SATURN_NETWORK }}"
+export SATURN_HOME=/mnt/md0/saturn
+./$1
