4d2fe35da08567f39499f8837ed10237de6c713b
YurkoWasHere <YurkoWasHere@users.noreply.github.com>
2022-09-29 13:00:00 -0400

    Add Loki Support Switch to Production Move program folder to saturn data

--- roles/saturn_l1/templates/init.sh
+++ roles/saturn_l1/templates/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd /mnt/data/saturn
 export FIL_WALLET_ADDRESS="{{ SATURN_FILECOIN_ADDRESS }}"
 export NODE_OPERATOR_EMAIL="{{ SATURN_EMAIL }}"
 export SATURN_NETWORK="{{ SATURN_NETWORK }}"
 export SATURN_HOME=/mnt/md0/saturn
-./$1
+/mnt/data/saturn/saturn-l1/$1 2> /dev/null  >> /mnt/data/saturn/l1-cron.log
